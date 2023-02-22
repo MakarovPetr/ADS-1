bdd528f537fe9b927516e009de7a870ec1463c9b
MakarovPetr <125215189+MakarovPetr@users.noreply.github.com>
2023-02-22 12:24:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,51 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+for (int i = 2; i < value; i++) {
+		if (value % i == 0)
+			return false;
+	}
+	return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+int count = 0;
+int a = 1;
+int	chislo = 0;
+	while (count != n) {
+		bool Flag = true;
+		a++;
+		for (int i = 2; i < a; i++) {
+			if (a % i == 0)
+				Flag = false;
+		}
+		if (Flag == true) {
+			chislo = a;
+			count++;
+		}
+	}
+	return chislo;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+while (1) {
+		value++;
+		if (checkPrime(value) == false) {
+			value++;
+            checkPrime(value);
+	    }
+		
+		if (checkPrime(value) == true)
+			return value;
+	}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+int summ = 0;
+	for (int i = 2; i < hbound; i++) {
+		if (checkPrime(i) == true) {
+			summ += i;
+		}
+	}
+	return summ;
 }
